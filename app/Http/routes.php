c314a32e1bd7f6f330dd1fd8667bb94982deffc8
KAVYASREES <kavya@imrokraft.com>
2016-10-12 13:55:25 -0700

    login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,25 +11,33 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/upload', ['as' => 'image.create', 'uses' => 'ImageController@create']);
-Route::post('/upload', ['as' => 'image.store' , 'uses' => 'ImageController@store']);
-
-
 Route::get('layout',function () {
-    return view('backend.event_gallery.upload_images');
+    return view('backend.blog.new_blog');
 });
 Route::get('/',function () {
     return view('index');
 });
 
+Route::get('login',function () {
+    return view('blog/new');
+});
+
+
+Route::get('event/new','EventGalleryController@create');
+Route::post('event/new','EventGalleryController@store');
+
+Route::get('event/gallery',['as'=>'image.create','uses'=>'ImageController@create']);
+Route::post('event/gallery',['as'=>'image.store','uses'=>'ImageController@store']);
+
 Route::auth();
+
+
 
 Route::get('/home', 'HomeController@index');
 
 Route::get('/blog/new', 'BlogController@new_blog');
 
-Route::post('store','BlogController@store');
+Route::post('/store','BlogController@store');
 
 Route::group(['middleware'=>['auth']],function(){
 });
