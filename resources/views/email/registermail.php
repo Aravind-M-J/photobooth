790d8fb7620aee26d8e69822034689640ca44df8
Chippymerinmathew <chippy@imrokraft.com>
2016-10-20 12:25:10 +0530

    REGISTER

---
+++ resources/views/email/registermail.php
@@ -0,0 +1,45 @@
+<html>
+<head></head>
+    <body>
+
+        <div class="mail1">
+            <div class="container">
+                <div class="col-md-12 col-sm-12 col-xs-12 abo"><br>
+                    <h2 class="mail_h">Party Crooks <span class="mail_h_col"> Order Form</span></h2>
+                </div>
+            </div>
+        </div>
+        <div class="gallery" id="gallery">
+            <div class="container">
+                <div class="gallery-main">
+                    <div class="col-md-12 blog-grid-left animated wow slideInLeft backgroundDiv" data-wow-delay=".5s">
+
+                        <div class="col-md-6 reg_form">
+                            <span class="reg_form">Client Name</span>
+                            {{$clientname}}
+                        </div>
+
+                        <div class="col-md-6 reg_form">
+                            <span class="reg_form">Contact number</span>
+                            {{$clientnumber}}
+                        </div>
+
+                        <div class="col-md-6 reg_form">
+                            <span class="reg_form">Address</span>
+                            {{$address}}
+                        </div>
+
+                        <div class="col-md-6 reg_form reg_form">
+                            <span class="reg_form">Email</span>
+                            {{$email}}
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
