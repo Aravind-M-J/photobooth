f42a9ae7902300c920f8f6935e3536176d4a6a0e
KAVYASREES <kavya@imrokraft.com>
2016-10-20 14:01:06 -0700

    Count

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -1,15 +1,51 @@
 @extends('backend.layouts.layout')
 @section('home','active')
 @section('body')
- @include('flash')
 
 <?php
     $user = \Illuminate\Support\Facades\Auth::user();
 ?>
+@section('title', 'admin')
 
-<div class="jumbotron">
+    @include('flash')
+
+    <div class="row">
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-aqua">
+                <div class="inner">
+                  <h3>Blog</h3>
+                   {{$data}}
+                </div>
+                <div class="icon">
+                  <i class="fa fa-user"></i>
+                </div>
+                {{--<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>--}}
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-green">
+                <div class="inner">
+                  <h3>Event</h3>
+                      {{$eventCount}}
+              
+                </div>
+                <div class="icon">
+                  <i class="fa fa-user"></i>
+                </div>
+                {{--<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>--}}
+              </div>
+            </div>
+            <!-- ./col -->
+           
+          </div>
+
+@endsection
+<!--<div class="jumbotron">
         <h1>Admin Page</h1>
         <p>This page is  admins only!</p>
-    </div>
+</div>-->
+
     
-@endsection
