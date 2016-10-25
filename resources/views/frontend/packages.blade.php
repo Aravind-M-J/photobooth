1d1c99a3071484a3f5d09215551f03edec1f6210
subin08 <subin@imrokraft.com>
2016-10-25 19:42:52 +0530

    packages

--- resources/views/frontend/packages.blade.php
+++ resources/views/frontend/packages.blade.php
@@ -16,66 +16,54 @@
 
 						<h2 class="abo_h"> Photography <span class="abo_h_col"> Price</span></h2>
 					</div>
-			<div class="pricing-plans-grids">
-				<div class="col-md-4 pricing-plans-grid animated wow slideInLeft" data-wow-delay=".5s">
-					<div class="pricing-plans-grid1">
-						
-						<h5>Basic package</h5>
-						<ul>
-							<li>Event will be on for 2 hours</li>
+			<!--
+        #pricing
+        ========================== -->
+        <section id="pricing">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInUp" >
+                        <div class="pricing-table text-center">
+                            <div class="price">
+                                <h2>Basic package</h2>
+                            </div>
+                            <ul class="text-center">
+                                <li>Event will be on for 2 hours</li>
 							<li>Photo booth</li>
 							<li>150 photo prints will be available</li>
 							<li>Complimentary gift will be given to the client</li>
-							<li>Props will include:
-								<ul>
-									<li>Fancy hats</li>
-									<li>Masks of different styles</li>
-									<li>Fancy hair props</li>
-									<li>Dialog box</li>
-									<li>Fancy glasses</li>
-									<li>Bows</li>
-								</ul>
-							</li>
-						</ul>
-						<!-- <div class="more m1">
-							<a href="single.html" class="hvr-shutter-in-vertical hvr-shutter-in-vertical1">Learn More...</a>
-						</div> -->
-					</div>
-				</div>
-				<div class="col-md-4 pricing-plans-grid animated wow slideInLeft" data-wow-delay=".6s">
-					<div class="pricing-plans-grid1">
-						
-						<h5>Standard package</h5>
-						<ul>
-							<li>Event will be on for 3 hours</li>
+							<li>Props will include:<br>  Fancy hats<br>  Masks of different styles<br>  Fancy hair props<br>Dialog box<br>Fancy glasses<br>Bows
+							
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInUp" data-wow-delay="0.3s">
+                        <div class="pricing-table text-center">
+                            <div class="price">
+                                <h2>Standard package</h2>
+                            </div>
+                            <ul class="text-center">
+                                <li>Event will be on for 3 hours</li>
 							<li>Custom theme Photo booth</li>
 							<li>300 photo prints will be available</li>
 							<li>Live games will be conducted during the event and winners will be awarded with gifts</li>
 							<li>Complimentary gift will be given to the client</li>
-							<li>Props will include:
-								<ul>
-									<li>Fancy hats</li>
-									<li>Masks of different styles</li>
-									<li>Fancy hair props</li>
-									<li>Dialog box</li>
-									<li>Fancy glasses</li>
-									<li>Bows</li>
-							<br>
+							<li>Props will include:<br>Fancy hats<br>Masks of different styles<br>Fancy hair props<br>Dialog box<br>Fancy glasses<br>Bows
 							<li>*This package will also include custom themed props</li>
 								</ul>
 							</li>
-						</ul>
-						<!-- <div class="more m1">
-							<a href="single.html" class="hvr-shutter-in-vertical hvr-shutter-in-vertical1">Learn More...</a>
-						</div> -->
-					</div>
-				</div>
-				<div class="col-md-4 pricing-plans-grid animated wow slideInLeft" data-wow-delay=".7s">
-					<div class="pricing-plans-grid1">
+                            </ul>
+                        </div>
+                    </div>
 
-						<h5>Premium package</h5>
-						<ul>
-							<li>Event will be on for 5 hours</li>
+                    <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="pricing-table text-center">
+                            <div class="price">
+                                <h2>Premium package</h2>
+                            </div>
+                            <ul class="text-center">
+                                <li>Event will be on for 5 hours</li>
 							<li>Custom theme Photo booth</li>
 							<li>500 photo prints will be available</li>
 							<li>Live games from a variety of choices will be conducted during the event and winners will be awarded with gifts</li>
@@ -84,36 +72,31 @@
 							<li>After video smash (maximum duration - 4 minutes)</li>
 							<li>Custom party cake</li>
 							<li>Complimentary gift will be given to the client</li>
-							<li>All Props will be custom themed. It will also include:
-								<ul>
-									<li>Fancy hats</li>
-									<li>Masks of different styles</li>
-									<li>Fancy hair props</li>
-									<li>Dialog box</li>
-									<li>Fancy glasses</li>
-									<li>Bows</li>
-								</ul>
-							</li>
-						</ul>
-						<!-- <div class="more m1">
-							<a href="single.html" class="hvr-shutter-in-vertical hvr-shutter-in-vertical1">Learn More...</a>
-						</div> -->
-					</div>
-				</div>
-            	<div class="col-md-12 pricing-plans-grid animated wow slideInLeft" data-wow-delay=".7s">
-            	<div class="pricing-plans-grid1 pricing-plans-grid2">
-				<h5>Additional Add-On Services</h5>
-                    <ul>
+							<li>All Props will be custom themed. It will also include:<br>Fancy hats<br>Masks of different styles<br>Fancy hair props<br>Dialog box<br>Fancy glasses<br>Bows
+                            </ul>
+                        </div>
+                    </div>
+					
+					<div class="col-md-12 col-sm-6 col-xs-12 wow fadeInUp break" data-wow-delay="0.3s">
+                        <div class="pricing-table text-center">
+                            <div class="price">
+                                <h2>Additional Add-On Services</h2>
+                            </div>
+							<ul>
                 	    <li>Dj night</li>
                 		<li>Karaoke</li>
                 		<li>Live music
                 		<li>Photo prints on mugs and frame</li>
                 		<li>Green screen</li>
                 	</ul>
-				</div>
-				</div>
-				<div class="clearfix"><p class="ps">PS: Package rate valid upto 30th November 2016</p></div>
-			</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--
+        End #pricing
+        ========================== -->
 		</div>
 	</div>
 <!-- //pricing-plans -->
